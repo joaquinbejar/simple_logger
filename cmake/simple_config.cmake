ea96a92d2c78c9cf20a735f47306804fc17e1264
Joaquin Bejar <jb@taunais.com>
2024-01-04 07:14:59 +0100

    VERSION: 0.1.6 update common

--- cmake/simple_config.cmake
+++ cmake/simple_config.cmake
@@ -1,7 +1,7 @@
 include(FetchContent)
 FetchContent_Declare(simple_config
         GIT_REPOSITORY https://github.com/joaquinbejar/simple_config.git
-        GIT_TAG v0.1.0
+        GIT_TAG dev
 )
 FetchContent_MakeAvailable(simple_config)
 if (CMAKE_DEBUG)
